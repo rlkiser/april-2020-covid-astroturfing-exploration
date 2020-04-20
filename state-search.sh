4ae7f3b515b82e4d2e84487ddc606772f0a03f08
ryanlkiser <ryanlkiser@gmail.com>
2020-04-20 18:54:51 -0400

    Added fixed bug which wasn't including full state names in search. Added complete search results.

--- state-search.sh
+++ state-search.sh
@@ -12,9 +12,9 @@ touch $REGISTRATIONS
 echo "File created on $DATE" > $REGISTRATIONS
 
 # Write whois records to individual files and write registration dates to dates file.
-for (( i = 0 ; i < 50 ; i++))
+for (( i = 0 ; i < 100 ; i++))
 do
-  WHOISREC=$(whois "ReOpen${STATES[$i]}.com")
+  WHOISREC=$(whois "reopen${STATES[$i]}.com")
   REGISTRATIONDATE=$(echo "$WHOISREC" | grep "Creation Date" | sed 's/^[ \t]*//;s/[ \t]*$//' | uniq)
 
   echo "$WHOISREC" > whois-records/${STATES[$i]}.txt
